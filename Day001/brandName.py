b5a790555f739ff1599825a940a4dfe22c411fc4
Kaushal kumar <kaushal221000@gmail.com>
2026-06-07 15:48:36 +0530

    Learned some basic of python

---
+++ Day001/brandName.py
@@ -0,0 +1,6 @@
+print("Welcome to brand Name Generator \n")
+city=input("Enter your city name: \n")
+pet=input("Enter your Pet Name: \n")
+
+print("Congrats you have generated brand Name:\n")
+print("your brand name is :",city," "+pet)
---
+++ Day001/input.py
@@ -0,0 +1,6 @@
+username=input("please Enter your Name:")
+print("\n")
+
+length=len(username)
+print(username)
+print("the length of your string is:",length)
